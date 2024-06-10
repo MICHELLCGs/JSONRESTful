5376269e3c0a6a71e73864a63290eae99a16a1c9
MICHELLCGs <michell.condori@tecsup.edu.pe>
2024-06-10 16:57:07 -0500

    proyecto

---
+++ JSONRESTful/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JSONRESTful
+//
+//  Created by Michell Condori on 10/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
